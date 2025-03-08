4d30790efc26c7628365b1c932c74f12c34d2159
choi2021 <juni2004c@gmail.com>
2025-03-08 17:11:39 +0900

    feat: socket 서버 연결

--- server.ts
+++ server.ts
@@ -1,10 +1,36 @@
 import express from "express";
 import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
-const config = require("./webpack.config.js");
+import http from "http";
+import { WebSocketServer } from "ws";
 
+const config = require("./webpack.config.js");
 const app = express();
+// HTTP 서버 생성
+const server = http.createServer(app);
 const compiler = webpack(config);
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (ws) => {
+  // 클라이언트에게 초기 메시지 전송
+  ws.send(
+    JSON.stringify({
+      type: "connected",
+      data: "WebSocket 서버에 연결되었습니다",
+    })
+  );
+
+  // 클라이언트 메시지 수신
+  ws.on("message", (message) => {
+    console.log("클라이언트로부터 메시지:", message.toString());
+  });
+
+  // 연결 종료 처리
+  ws.on("close", () => {
+    console.log("클라이언트 연결이 종료되었습니다");
+  });
+});
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
@@ -14,6 +40,6 @@ app.use(
   })
 );
 
-app.listen(3000, function () {
+server.listen(3000, function () {
   console.log("Example app listening on port 3000!\n");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.ts",
+  entry: [
+    // HMR 클라이언트 추가
+    path.resolve(__dirname, "src/socketClient.ts"),
+    // 기존 진입점
+    "./src/index.ts",
+  ],
   module: {
     rules: [
       {
